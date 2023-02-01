2339215221f1ec3e82fe3b69325cfdb32da4ef19
Ramago1715 <74196893+Ramago1715@users.noreply.github.com>
2023-02-02 00:23:35 +0100

    Update MainLectura.py

--- MainLectura.py
+++ MainLectura.py
@@ -2,3 +2,8 @@
 #usar split() para separar las frases en sus palabras en una lista
 #meter todas esas palabras en un diccionario como key
 #y como valor metemos una lista metemos todas las palabras que van despues de esta(repetidas incluidas)
+from MetodosLectura import *
+
+si = lectura_archvio("prueba.txt")
+#meter_palabras_en_diccionario(si)
+depurar_frase(si)
