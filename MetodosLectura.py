78060a64836668d01663548c652c65b5dce161b9
Ramago1715 <74196893+Ramago1715@users.noreply.github.com>
2023-02-02 00:24:13 +0100

    Update MetodosLectura.py

--- MetodosLectura.py
+++ MetodosLectura.py
@@ -1,8 +1,22 @@
-def leerarchivo(archivo,formato):
-    f = open(archivo,formato)
-    lineas = []
-    for linea in f:
-        lineas.append(linea.split())
-    print(len(lineas),lineas)
+def lectura_archvio(archivo):
+    f = open(archivo,encoding="windows-1252")
+    return f
+    
+def depurar_frase(archivo):
+    texto_limpio = archivo
+    textolimp = texto_limpio.translate(str.maketrans('.?,-`´></,','',string.punctuation))
+    print(texto_limpio)
 
-leerarchivo("frases_informatica.txt","rt")
+def meter_palabras_en_diccionario(f):
+    from collections import defaultdict
+    lineas = defaultdict(list)
+    for linea in f:
+            frase = linea.split()
+            for i in range(0,len(frase)-1):
+                    if i == 0:
+                        lineas["INICIO"].append(frase[i])
+                    lineas[frase[i]].append(frase[i+1])
+    print(lineas)
+
+#al crear las frases no pueden ser frases repetidas ni que existen en el doc original
+#random.choice(lista)
