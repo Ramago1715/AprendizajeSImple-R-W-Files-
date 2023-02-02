36256092728f69a873f67cea853800e65067a20f
Ramago1715 <74196893+Ramago1715@users.noreply.github.com>
2023-02-02 03:47:43 +0100

    Update MetodosLectura.py

--- MetodosLectura.py
+++ MetodosLectura.py
@@ -1,22 +1,29 @@
-def lectura_archvio(archivo):
-    f = open(archivo,encoding="windows-1252")
-    return f
-    
-def depurar_frase(archivo):
-    texto_limpio = archivo
-    textolimp = texto_limpio.translate(str.maketrans('.?,-`´></,','',string.punctuation))
-    print(texto_limpio)
+def depurar_frase(archivo,documento_limpio):
+    si = '.?,¿!-_<>'
+    texto_limpio = open(documento_limpio, 'a')
+    with open(archivo) as texto:
+        for linea in texto:
+            linea_limpia = linea.translate(str.maketrans(" "," ", si))
+            texto_limpio.write(linea_limpia)
+    texto_limpio.close()
 
+    
 def meter_palabras_en_diccionario(f):
+    import os
     from collections import defaultdict
+    x = open(f,"r")
     lineas = defaultdict(list)
-    for linea in f:
-            frase = linea.split()
-            for i in range(0,len(frase)-1):
-                    if i == 0:
-                        lineas["INICIO"].append(frase[i])
-                    lineas[frase[i]].append(frase[i+1])
+    for linea in x:
+        frase = linea.split()
+        for i in range(0,len(frase)-1):
+            if i == 0:
+                lineas["INICIO"].append(frase[i])
+            lineas[frase[i]].append(frase[i+1])
     print(lineas)
+    x.close()
+    os.remove("archivolimpio.txt")
+    
+    
 
 #al crear las frases no pueden ser frases repetidas ni que existen en el doc original
 #random.choice(lista)
